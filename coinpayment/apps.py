9db0709f505c2ef773a57f4116054c2a85420d29
Gautham N <mohit_creed@yahoo.com>
2019-03-18 10:43:43 +0530

    removed

--- coinpayment/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class CoinpaymentConfig(AppConfig):
-    name = 'coinpayment'
--- coinpayment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name='coinpayment'
-
-urlpatterns = [
-	path('', views.submitform_view, name='submitform'),
-    path('history/',views.profile_view, name='history')
-]	
